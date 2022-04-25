08d71aa5feb600122231f4d256db3e691d5ccd99
Tomás Elizondo <tomaslelizondo@gmail.com>
2022-04-25 12:34:45 -0300

    hyperscript implemented

--- src/index.js
+++ src/index.js
@@ -1,30 +1,20 @@
+import jsx from "hyperscript"; // En react le agregaron un decorador para que se pueda escribir como html, pero en el fondo usa esto.
 import { registerImage } from "./lazy";
 
 const random = (minimum, maximum) => Math.floor(Math.random() * (maximum - minimum)) + minimum
 
-const createImageNode = () => {
-  const container = document.createElement('div');
-  container.className = "p-4";
-  
-  const imagen = document.createElement('img');
-  imagen.className = "mx-auto object-cover h-80 w-80 bg-gray-400 rounded";
-  // imagen.width = "320";
-  imagen.dataset.src = `https://randomfox.ca/images/${random(1, 122)}.jpg`; // TODO
+const createImageNode = () => {  
+  const imagen = jsx("img.mx-auto.object-cover.h-80.w-80.bg-gray-400.rounded", {
+    "data-src": `https://randomfox.ca/images/${random(1, 122)}.jpg`
+  });
 
-  
-  container.appendChild(imagen);
+  const container = jsx("div.p-4", imagen);
   
   return container;
-  {/*
-    <div class="p-4">
-      <img class="mx-auto" width="320" src="https://randomfox.ca/images/2.jpg" alt="">
-    </div>
-  */}
 };
 
 const mountNode = document.getElementById('images')
 
-// Add fox
 const button = document.getElementById("addFox");
 const addFoxAction = () =>  {
   const newImage = createImageNode();
